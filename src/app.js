60a0222b677751d4e37c7fe74e3bd029c4b65154
Sri Ritwik <ritwiksri3@gmail.com>
2021-07-03 16:24:58 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,47 +1,34 @@
-import React, { useState, useEffect } from "react";
-import Loading from "./loading";
-import Posts from "./posts";
-
-const url =
-  "https://raw.githubusercontent.com/sriritwik3/users-api-server/master/db.json";
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-      setLoading(false);
-      setData(data.facts);
-    } catch (e) {
-      setLoading(false);
-      console.log("error", e);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    );
-  }
-  console.log(data);
-  if (data.length !== 0)
-    return (
-      <main>
-        <Posts posts={data}></Posts>
-      </main>
-    );
-  else return <h1>BE HAPPY MAMA</h1>;
+  const [markdown, setMarkdown] = useState("# Markdown Previewer");
+  return (
+    <div>
+      <div className="markdown">
+        <Container fluid>
+          <Row>
+            <Col>
+              <div>
+                <textarea
+                  className="input"
+                  value={markdown}
+                  onChange={(e) => setMarkdown(e.target.value)}
+                ></textarea>
+              </div>
+            </Col>
+            <Col>
+              <div className="preview">
+                <ReactMarkdown>{markdown}</ReactMarkdown>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default App;
