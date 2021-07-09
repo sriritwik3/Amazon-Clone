36549abb45d99e4078bcb0b882541e32ef1782e9
Sri Ritwik <ritwiksri3@gmail.com>
2021-07-09 16:58:36 +0530

    made cart-count dynamic

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,44 @@
 import React, { useState, useEffect } from "react";
 import "./Header.css";
+import { useGlobalContext } from "./Context";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 const Header = () => {
   const [isFocused, setIsFocused] = useState(false);
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const [{ cart }] = useGlobalContext();
   const [outlineStyle, setOutlineStyle] = useState({
     boxShadow: "0 0 0 2pt orangered",
+    clipPath: "inset(-2pt -2pt -2pt 0)",
   });
   const onBlur = () => {
-    console.log("onblurloki vachamowa");
     setIsFocused(false);
   };
   const onFocus = () => {
-    console.log("onfocus loki vacha MOWA");
     setIsFocused(true);
+  };
+  const onMouseDown = () => {
+    setIsMouseOver(true);
+  };
+  const onMouseUp = () => {
+    setIsMouseOver(false);
   };
   useEffect(() => {
     isFocused
-      ? setOutlineStyle({ boxShadow: "0 0 0 2pt orangered" })
+      ? setOutlineStyle({
+          boxShadow: "0 0 0 2pt orangered",
+          clipPath: "inset(-2pt -2pt -2pt 0)",
+        })
       : setOutlineStyle({ boxShadow: "none" });
   }, [isFocused]);
+  useEffect(() => {
+    isMouseOver
+      ? setOutlineStyle({
+          boxShadow: "0 0 0 2pt orangered",
+        })
+      : setOutlineStyle({ boxShadow: "none" });
+  }, [isMouseOver]);
 
   return (
     <>
@@ -34,13 +52,20 @@ const Header = () => {
         </div>
         <div className="nav_fill">
           <div className="nav_search">
-            <input type="text" className="nav_search_input" />
-            <SearchIcon
-              className="nav_search_icon"
-              style={outlineStyle}
+            <input
+              type="text"
+              className="nav_search_input"
               onFocus={onFocus}
               onBlur={onBlur}
-            ></SearchIcon>
+            />
+            <div onFocus={onFocus} onBlur={onBlur}>
+              <SearchIcon
+                className="nav_search_icon"
+                style={outlineStyle}
+                onMouseDown={onMouseDown}
+                onMouseUp={onMouseUp}
+              ></SearchIcon>
+            </div>
           </div>
         </div>
         <div className="nav_right">
@@ -53,7 +78,7 @@ const Header = () => {
             <div className="line2">&amp;Orders</div>
           </div>
           <div className="nav_cart">
-            <span className="cart_count">0</span>
+            <span className="cart_count">{cart.length}</span>
             <ShoppingCartIcon className="cart_icon"></ShoppingCartIcon>
           </div>
         </div>
